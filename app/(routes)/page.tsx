fe33c78a712f38c917b57c43d94f747d3e32a0ad
Sara Öhman <saraohman97@gmail.com>
2023-12-30 01:10:04 +0100

    single product page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,18 +1,25 @@
 import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
 import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
 import Container from "@/components/ui/container";
 
 export const revalidate = 0;
 
 const HomePage = async () => {
-    const billboard = await getBillboard("3920f20b-0f36-45da-91b8-b5d0d1bcd0f5")
-    return ( 
-        <Container>
-            <div className='space-y-10 pb-10'>
-                <Billboard data={billboard} />
-            </div>
-        </Container>
-     );
-}
- 
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("3920f20b-0f36-45da-91b8-b5d0d1bcd0f5");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
+};
+
 export default HomePage;
